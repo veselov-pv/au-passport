99f8f656d8f716225871a747a55bad13cb27b97c
veselov-pv <7262830@gmail.com>
2015-04-03 02:27:32 +0300

    nodejs cors. '/' to static client

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-var express = require('express');
+var express = require('express'),
+	cors = require('cors');
 var app = express();
+
+app.use(cors());
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use('/', express.static('client'));
 
-app.get('/', function(request, response) {
+/*app.get('/', function(request, response) {
   response.send('Hello World!');
-});
+});*/
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
