58a50f9b85d5da2a5e4c20a59b89874a79826ef5
veselov-pv <7262830@gmail.com>
2015-04-08 01:43:05 +0300

    templeting

--- index.js
+++ index.js
@@ -5,12 +5,43 @@ var app = express();
 app.use(cors());
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-app.use('/', express.static('client'));
 
-/*app.get('/', function(request, response) {
-  response.send('Hello World!');
-});*/
+app.use(express.static(__dirname + '/views'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.render('pages/home');
+});
+
+app.get('/home', function(req, res) {
+	res.render('pages/home');
+});
+
+app.get('/order', function(req, res) {
+	res.render('pages/order');
+});
+
+app.get('/edit', function(req, res) {
+	res.render('pages/edit');
+});
+
+app.get('/checkout', function(req, res) {
+	res.render('pages/checkout');
+});
+
+app.get('/confirmation', function(req, res) {
+	res.render('pages/confirmation');
+});
+
+app.get('/tips', function(req, res) {
+	res.render('pages/tips');
+});
+
+app.get('/feedback', function(req, res) {
+	res.render('pages/feedback');
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
